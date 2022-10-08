9ff66828ed1813393aea036c1f94ed272f30803b
Bennen Tun <alexandretun96@gmail.com>
2022-10-07 19:56:32 -0500

    detalles Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,16 +12,26 @@ public class Main {
           System.out.println(obj1);
         System.out.println(obj);
 
-        ArrayList<Asignatura> catalogo = new ArrayList<Asignatura>();
-        catalogo.add(new Asignatura("Álgebra Intermedia", 1,8,10));
-        catalogo.add(new Asignatura("Geometría Analítica", 1,8,12));
-        catalogo.add(new Asignatura("Algoritmia", 1,8,13));
-        catalogo.add(new Asignatura("Fundamentos de Ingeniería de Software", 1,8,14));
-        catalogo.add(new Asignatura("Responsabilidad Social Universitaria", 1,8,15));
-
-        for (Asignatura x: catalogo) {
-            System.out.println(x);
+       ArrayList<Asignatura> lista = new ArrayList<Asignatura>();
+        lista.add(new Asignatura("Álgebra Intermedia", 1,8,10));
+        lista.add(new Asignatura("Geometría Analítica", 1,8,12));
+        lista.add(new Asignatura("Algoritmia", 1,8,13));
+        lista.add(new Asignatura("Fundamentos de Ingeniería de Software", 1,8,14));
+        lista.add(new Asignatura("Responsabilidad Social Universitaria", 1,8,15));
+        for(int i = 0; i<lista.size() ; i++) {
+            System.out.println(lista.get(i));
         }
+
+        /*Asignatura[] plantilla = new Asignatura[5];
+        plantilla[0] = new Asignatura("Álgebra Intermedia", 1,8,10);
+        plantilla[1] = new Asignatura("Geometría Analítica", 1,8,12);
+        plantilla[2] = new Asignatura("Algoritmia", 1,8,13);
+        plantilla[3] = new Asignatura("Fundamentos de Ingeniería de Software", 1,8,14);
+        plantilla[4] = new Asignatura("Responsabilidad Social Universitaria", 1,8,15);
+
+        for (Asignatura x: plantilla) {
+            System.out.println(x);
+        }/*
 
 
 
