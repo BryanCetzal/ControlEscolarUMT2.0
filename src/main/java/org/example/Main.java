7b530310b5e83b765a9725e400bfbcae6f6dd6d6
EmmanuelCetzal <bryanemmanuel1209@gmail.com>
2022-10-07 19:23:42 -0500

    Controladores modificados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,14 +12,21 @@ public class Main {
           System.out.println(obj1);
         System.out.println(obj);
 
-        ArrayList<Asignatura> catalogo = new ArrayList<>(Asignatura);
+       /* ArrayList<Asignatura> catalogo = new ArrayList<>(Asignatura);
         catalogo.add(new Asignatura("Álgebra Intermedia", 1,8,10));
         catalogo.add(new Asignatura("Geometría Analítica", 1,8,12));
         catalogo.add(new Asignatura("Algoritmia", 1,8,13));
         catalogo.add(new Asignatura("Fundamentos de Ingeniería de Software", 1,8,14));
-        catalogo.add(new Asignatura("Responsabilidad Social Universitaria", 1,8,15));
+        catalogo.add(new Asignatura("Responsabilidad Social Universitaria", 1,8,15));*/
 
-        for (Asignatura x: catalogo) {
+        Asignatura[] plantilla = new Asignatura[5];
+        plantilla[0] = new Asignatura("Álgebra Intermedia", 1,8,10);
+        plantilla[1] = new Asignatura("Geometría Analítica", 1,8,12);
+        plantilla[2] = new Asignatura("Algoritmia", 1,8,13);
+        plantilla[3] = new Asignatura("Fundamentos de Ingeniería de Software", 1,8,14);
+        plantilla[4] = new Asignatura("Responsabilidad Social Universitaria", 1,8,15);
+
+        for (Asignatura x: plantilla) {
             System.out.println(x);
         }
 
